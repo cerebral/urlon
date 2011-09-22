6aa8f80cb10d1c86c55c273700450282a6d286b1
Christopher Chedeau <vjeuxx@gmail.com>
2011-09-22 12:36:19 +0300

    Handle URI encoding as well as escape mechanism

--- urlon.js
+++ urlon.js
@@ -1,37 +1,48 @@
 URLON = {
 	stringify: function (input) {
-		// Number or Boolean or Null
-		if (typeof input === 'number' || input === true || input === false || input === null) {
-			return ':' + input;
+		function encodeString (str) {
+			return encodeURI(str.replace(/([=:&#_,])/g, '$1$1'));
 		}
-		// Array
-		if (input instanceof Array) {
-			var str = '#';
-			for (var i = 0; i < input.length; ++i) {
-				str += URLON.stringify(input[i]) + ',';
+		
+		function stringify (input) {
+			// Number or Boolean or Null
+			if (typeof input === 'number' || input === true || input === false || input === null) {
+				return ':' + input;
 			}
-			return str.substring(0, str.length - 1);
-		}
-		// Object
-		if (typeof input === 'object') {
-			var str = '_';
-			for (var key in input) {
-				str += key + URLON.stringify(input[key]) + '&';
+			// Array
+			if (input instanceof Array) {
+				var str = '#';
+				for (var i = 0; i < input.length; ++i) {
+					str += stringify(input[i]) + ',';
+				}
+				return str.substring(0, str.length - 1);
 			}
-			return str.substring(0, str.length - 1);
+			// Object
+			if (typeof input === 'object') {
+				var str = '_';
+				for (var key in input) {
+					str += encodeString(key) + stringify(input[key]) + '&';
+				}
+				return str.substring(0, str.length - 1);
+			}
+			// String
+			return '=' + encodeString(input.toString());
 		}
-		// String
-		return '=' + input.toString();
 	},
-
+	
 	parse: function (str) {
 		var pos = 0;
+		str = decodeURI(str);
 
 		function read() {
 			var token = '';
 			for (; pos !== str.length; ++pos) {
 				if (str[pos].match(/[=:&#_,]/)) {
-					break;
+					if (str[pos + 1] === str[pos]) {
+						pos += 1;
+					} eles {
+						break;
+					}
 				}
 				token += str[pos];
 			}
